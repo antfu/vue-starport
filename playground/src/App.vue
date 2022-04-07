ff44ba963881ec57a2d1bb253614b1061b0b688b
Simon he <13917107460@163.com>
2022-04-07 09:36:21 +0800

    chore:hidden scrollbar

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,3 +1,9 @@
+<!--
+ * @Author: Simon
+ * @Date: 2022-04-06 23:08:26
+ * @LastEditTime: 2022-04-07 09:33:55
+ * @FilePath: \vue-starport\playground\src\App.vue
+-->
 <script setup lang="ts">
 import { StarportCarrier } from 'vue-starport'
 </script>
@@ -16,3 +22,19 @@ import { StarportCarrier } from 'vue-starport'
   </main>
   <StarportCarrier />
 </template>
+
+<style lang="css" >
+::-webkit-scrollbar{
+  background:transparent;
+  width: 0;
+  height: 10px;
+}
+::-webkit-scrollbar-thumb{
+  border-radius: 5px;
+  background-color: #e1e1e1;
+  width: 0;
+  height: 6px;
+  border: 2px solid transparent;
+  background-clip: content-box;
+}
+</style>
