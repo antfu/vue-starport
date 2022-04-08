a248115124ff11666c2757d89d786925eec02be8
Simon he <13917107460@163.com>
2022-04-08 17:09:02 +0800

    fix: page shake

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+import './index.css'
 
 const app = createApp(App)
 const router = createRouter({
