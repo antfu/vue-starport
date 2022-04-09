7be7f70a8ade7cf1bda311037de799fa44020a4c
Simon he <13917107460@163.com>
2022-04-09 20:20:38 +0800

    chore: add e type

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-import type { Component, StyleValue } from 'vue'
+import type { ClickEvent, Component, StyleValue } from 'vue'
 import { Teleport, computed, defineComponent, h, nextTick, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import type { StarportContext } from './context'
 import { createStarportContext } from './context'
@@ -174,7 +174,7 @@ export function createStarport<T extends Component>(
   const board = defineComponent({
     name: `starport-board-${componentId}`,
     setup() {
-      const listenUrl = (e) => {
+      const listenUrl = (e: ClickEvent) => {
         const { pathname } = e.currentTarget.activeElement
         if (!pathname)
           return
