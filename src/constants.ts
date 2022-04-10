21ac26c135cd1175209e0d82783ee536ce42eb95
tangdaoyuan <1197633750@qq.com>
2022-04-10 21:28:55 +0800

    refactor: Symbol for avoiding polluting provider

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ import type { InjectionKey } from 'vue'
 import type { StarportOptions } from './types'
 import type { createInternalState } from './state'
 
-export const InjectionState = 'vue-starport-injection' as unknown as InjectionKey<ReturnType<typeof createInternalState>>
-export const InjectionOptions = 'vue-starport-options' as unknown as InjectionKey<StarportOptions>
+export const InjectionState: InjectionKey<ReturnType<typeof createInternalState>> = Symbol('vue-starport-injection')
+export const InjectionOptions: InjectionKey<StarportOptions> = Symbol('vue-starport-options')
